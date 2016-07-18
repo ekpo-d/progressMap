1fd060ea95d5a16d18b911c7d9b4a10ccf3ddb3c
Ekpo <ekpodave@rocketmail.com>
2016-07-18 07:17:09 +0100

    updated add view

--- progressMap/add/views.py
+++ progressMap/add/views.py
@@ -2,7 +2,7 @@ from flask import render_template, abort
 from . import add, forms
 
 
-@add.route('/' methods=['GET', 'POST'])
+@add.route('/', methods=['GET', 'POST'])
 def main():
 	form = forms.addForm()
 	return render_template('add.html', form = form)
