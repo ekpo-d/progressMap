d80fd89a7a9603cef3bb229ce0fc1597421ae751
Ekpo <ekpodave@rocketmail.com>
2016-07-22 10:26:15 +0100

    made small updates to templates and user view

--- progressMap/user/views.py
+++ progressMap/user/views.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from . import user
+from .. import models
 
 @user.route('/<username>')
 def userPage(username):
-	return render_template('user.html', username = username)
+	user = models.User.query.filter_by(username=username).first_or_404()
+	return render_template('user.html', user = user)
