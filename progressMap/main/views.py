844de5b78a8c2d6444468b4cd07bfc265efe0639
Ekpo <ekpodave@rocketmail.com>
2016-07-22 22:04:22 +0100

    updated  login_manager to main view

--- progressMap/main/views.py
+++ progressMap/main/views.py
@@ -1,7 +1,11 @@
-from flask import render_template, abort, redirect, url_for
+from flask import render_template, abort, redirect, url_for, request, flash
 from flask_login import login_user
-from .. import models
+from .. import models, login_manager
 from . import main, forms
+
+@login_manager.user_loader
+def load_user(userid):
+	return models.User.query.get(int(userid))
 
 @main.route('/', defaults={'page' : 'index'})
 @main.route('/<page>', methods=['GET', 'POST'])
@@ -15,8 +19,11 @@ def show(page):
 		if form.validate_on_submit():
 			user = models.User.query.filter_by(username=form.username.data).first()
 			if user is not None:
-				login_user(user, form,remember_me.data)
-				return redirect (url_for('.show', page='map'))
+				login_user(user, form.remember_me.data)
+				flash('Login Successful.')
+				return redirect (request.args.get('next') or url_for('.show', page='map'))
+			flash('Incorrect username or password.')
+			return redirect(url_for('main.show', page='login'))
 		return render_template('login.html', form=form)
 	elif page == 'signup':
 		return render_template('signup.html')
