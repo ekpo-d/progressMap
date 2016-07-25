d72586fc3ebd1ab5667da6d6d1fe09d0d7c76935
Ekpo <ekpodave@rocketmail.com>
2016-07-25 22:19:17 +0100

    updated curriculum list heading

--- progressMap/static/js/main.js
+++ progressMap/static/js/main.js
@@ -2,3 +2,9 @@
   $(".navbar-toggle").on("click", function () {
 				    $(this).toggleClass("active");
     });
+
+//curriculum list headings
+curriculum = $('.curriculum')
+curriculum.map(function(element){
+	curriculum[element].textContent = curriculum[element].textContent.toUpperCase()
+})
