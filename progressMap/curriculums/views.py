9df76aeaaa108c022f45dfb0c9746ab17b9c07e2
Ekpo <ekpodave@rocketmail.com>
2016-07-27 17:10:07 +0100

    updated curriculum view and template

--- progressMap/curriculums/views.py
+++ progressMap/curriculums/views.py
@@ -8,8 +8,8 @@ def show(page):
 	if page == 'all':
 		curriculumList = models.getFromDb(models.Curriculums, 6)
 		return render_template('curriculums.html', curriculumList=curriculumList)
-	elif page == models.get_by_title(models.Curriculums, page).title:
-		curriculum = models.get_by_title(models.Curriculums, page)
+	elif page == models.getByTitle(models.Curriculums, page).title:
+		curriculum = models.getByTitle(models.Curriculums, page)
 		return render_template('showCurriculum.html', curriculum=curriculum)
 	else:
 		abort(404)
