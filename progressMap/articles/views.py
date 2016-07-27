ff71e8c746ef59d737fb8fe03aaf8ea5fa0dd68e
Ekpo <ekpodave@rocketmail.com>
2016-07-27 17:23:18 +0100

    updated view

--- progressMap/articles/views.py
+++ progressMap/articles/views.py
@@ -2,7 +2,14 @@ from flask import render_template
 from . import articles
 from .. import models
 
-@articles.route('/')
-def show():
-	articlesList = models.getFromDb(models.Articles, 6)
-	return render_template('articles.html', articlesList=articlesList)
+@articles.route('/', defaults={'page':'all'})
+@articles.route('/<page>')
+def show(page):
+	if page == 'all':
+		articlesList = models.getFromDb(models.Articles, 6)
+		return render_template('articles.html', articlesList=articlesList)
+	elif page == models.getByTitle(models.Articles, page).title:
+		course = models.getByTitle(models.Articles, page)
+		return render_template('showArticle.html', article=article)
+	else:
+		abort(404)
