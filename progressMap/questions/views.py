bed9500995798d9795ac675f7b2af5b66dfb6656
Ekpo David <ekpodave@rocketmail.com>
2016-07-30 20:50:46 +0100

    updated view

--- progressMap/questions/views.py
+++ progressMap/questions/views.py
@@ -6,7 +6,8 @@ from .. import models
 
 @questions.route('/')
 def show():
-	return render_template('questions.html')
+	allQuestions = models.getFromDb(models.Questions, 6)
+	return render_template('questions.html', allQuestions)
 
 @questions.route('/ask', methods=['GET', 'POST'])
 @login_required
