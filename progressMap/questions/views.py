305450552cdddbb208c9efd26f96883202e6b00b
Ekpo David <ekpodave@rocketmail.com>
2016-07-31 09:23:29 +0100

    updated view

--- progressMap/questions/views.py
+++ progressMap/questions/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, abort
+from flask import render_template, abort, flash, redirect, url_for
 from flask_login import login_required, current_user
 from . import questions, forms
 from .. import models
