d061c642a9755e06ff4e59046b359a938dc51c67
Sama <gbozee@gmail.com>
2016-08-15 21:47:08 +0100

    refractored some views in main module. moved some evaluations to the forms.py file and cleaned up a particular view

---
+++ progressMap/settings.py
@@ -0,0 +1,11 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DEBUG=True
+    SECRET_KEY='\xcbP\x08\xf1\x9c\xe8\x9fq\x19\xf7\xf9\xb4\x05\xe3\xbc\x98A\x04\x9fw\xfa\t\x1cN'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'models.db')
+
+class DevelopmentConfig(Config):
+    DEBUG_TB_INTERCEPT_REDIRECTS=False
