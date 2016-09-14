70ce90a1ad7493f9705e455da7b8066982c45dc5
Ekpo David <ekpodave@rocketmail.com>
2016-09-14 10:26:30 +0100

    update

---
+++ progressMap/curriculums/views.py
@@ -0,0 +1,17 @@
+from flask import render_template, abort
+from . import curriculums
+from .. import models
+
+
+
+@curriculums.route('/', defaults={'page': 'all'})
+@curriculums.route('/<page>')
+def show(page):
+	if page == 'all':
+		curriculumList = models.getFromDb(models.Curriculums, 6)
+		return render_template('curriculums.html', curriculumList=curriculumList)
+	elif page == models.getByTitle(models.Curriculums, page).title:
+		curriculum = models.getByTitle(models.Curriculums, page)
+		return render_template('showCurriculum.html', curriculum=curriculum)
+	else:
+		abort(404)
